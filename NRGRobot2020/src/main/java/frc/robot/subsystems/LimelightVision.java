3749ce36a57a13e1c13faefa433e8cee3a6753e4
Naomi Bashkansky <nomichka@gmail.com>
2020-02-07 18:59:19 -0800

    Calculating values to get to inner goal

--- NRGRobot2020/src/main/java/frc/robot/subsystems/LimelightVision.java
+++ NRGRobot2020/src/main/java/frc/robot/subsystems/LimelightVision.java
@@ -30,6 +30,8 @@ public class LimelightVision extends SubsystemBase {
   NetworkTableEntry ts = table.getEntry("ts");
   NetworkTableEntry tshort = table.getEntry("tshort");
   NetworkTableEntry tlong = table.getEntry("tlong");
+  NetworkTableEntry thor = table.getEntry("thor");
+  NetworkTableEntry tvert = table.getEntry("tver");
   // read values periodically
   boolean averageDistancebool = false;
   double x = tx.getDouble(0.0);
@@ -38,6 +40,8 @@ public class LimelightVision extends SubsystemBase {
   double skew = ts.getDouble(0.0);
   double z = tshort.getDouble(0.0);
   double tLong = tlong.getDouble(0.0);
+  double width = thor.getDouble(0.0);
+  double height = tvert.getDouble(0.0);
   double distanceUsingTshort = (4822 / z) - 5.0664;
   double distanceTLong = (-0.024 + Math.sqrt(-0.0148 * tLong + 1.2543432)) / 0.0074;
   double AngledDistance = Math.sqrt(Math.pow(distanceUsingTshort, 2) + Math.pow(distanceTLong, 2));
@@ -56,12 +60,28 @@ public class LimelightVision extends SubsystemBase {
     return x;
   }
 
+  public double getDistance() {
+    return distance;
+  }
+
+  public double getWidth() {
+    return width;
+  }
+
+  public double getHeight() {
+    return height;
+  }
+
+  public double getSkew() {
+    return skew;
+  }
+
   @Override
   public void periodic() {
     SmartDashboard.putNumber("LimelightX", x);
     SmartDashboard.putNumber("LimelightY", y);
     SmartDashboard.putNumber("LimelightArea", area);
-    SmartDashboard.putNumber("LimeLIghtSkew", skew);
+    SmartDashboard.putNumber("LimelightSkew", skew);
     SmartDashboard.putNumber("Tshort", z);
     SmartDashboard.putNumber("DistanceUsingTshort", distanceUsingTshort);
     SmartDashboard.putNumber("AverageDistance", average);
