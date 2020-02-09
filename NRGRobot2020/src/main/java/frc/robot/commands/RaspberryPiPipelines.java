e4c8382e1fbcaeda0c21562a0d28e4eabfdea68b
Ed Reed <reed.ej@hotmail.com>
2020-02-09 11:34:51 -0800

    Extend CommandBase

--- NRGRobot2020/src/main/java/frc/robot/commands/RaspberryPiPipelines.java
+++ NRGRobot2020/src/main/java/frc/robot/commands/RaspberryPiPipelines.java
@@ -1,30 +1,36 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.RaspberryPiVision;
 import frc.robot.subsystems.RaspberryPiVision.PipelineRunner;
 
 /**
  * Add your docs here.
  */
-public class RaspberryPiPipelines {
+public class RaspberryPiPipelines extends CommandBase {
     private final RaspberryPiVision raspPi;
     private final PipelineRunner runner;
     public RaspberryPiPipelines(RaspberryPiVision raspPi, PipelineRunner runner) {
         this.raspPi = raspPi;
         this.runner = runner;
+        addRequirements(raspPi);
     }
 
+    @Override
     public void initialize() {
         raspPi.setPipelineRunner(runner);
     }
 
+    @Override
     public void execute() {
     }
 
-    public void end() {
+    @Override
+    public void end(boolean interrupted) {
 
     }
 
+    @Override
     public boolean isFinished() {
         return true;
     }
