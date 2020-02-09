429d709d88feec71ce6833ab7eebc3d14db54b9e
Paul Davis <pauldavis@google.com>
2020-02-08 16:33:41 -0800

    Update Feeder.java
    Add comments, remove copyright.

--- NRGRobot2020/src/main/java/frc/robot/subsystems/Feeder.java
+++ NRGRobot2020/src/main/java/frc/robot/subsystems/Feeder.java
@@ -1,27 +1,25 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
+/**
+ * Subsystem which commands the ball feeder.
+ * 
+ * The ball feeder is a single motorized wheel that pushes balls up into contact
+ * with the shooter flywheel.
+ */
 public class Feeder extends SubsystemBase {
-  private Victor feederVictor = new Victor(6);
+  private Victor feederMotor = new Victor(6);
 
   /**
-   * Creates a new Feeder.
+   * Creates the Feeder subsystem.
    */
   public Feeder() {
-
   }
 
   public void rawFeeder(double power){
-    feederVictor.set(power * 0.5);
+    feederMotor.set(power);
   }
 
   @Override
