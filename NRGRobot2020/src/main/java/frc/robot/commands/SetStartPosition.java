22f22b2806c7cfbdbb29ff77f6354b135ce3dab4
Naomi Bashkansky <nomichka@gmail.com>
2020-02-15 11:38:50 -0800

    Create SetStartPosition.java

---
+++ NRGRobot2020/src/main/java/frc/robot/commands/SetStartPosition.java
@@ -0,0 +1,47 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drive;
+
+public class SetStartPosition extends CommandBase {
+  private Drive drive;
+  private Pose2d pose;
+  /**
+   * Creates a new SetStartPosition.
+   */
+  public SetStartPosition(Drive drive, Pose2d pose) {
+    this.drive = drive;
+    this.pose = pose;
+    addRequirements(drive);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    drive.resetOdometry(pose);
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}
