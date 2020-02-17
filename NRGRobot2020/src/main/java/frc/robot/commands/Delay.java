b7dc1ea7c73aa0999ca761d1c677bcd4b8a602d9
Firenibs <natemg4@outlook.com>
2020-02-16 16:23:37 -0800

    Added Delay command

---
+++ NRGRobot2020/src/main/java/frc/robot/commands/Delay.java
@@ -0,0 +1,46 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Delay extends CommandBase {
+  /**
+   * Creates a new Delay.
+   */
+  double seconds;
+  Timer timer;
+
+  public Delay(double seconds) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.seconds = seconds;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    this.timer = new Timer();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return timer.get() >= seconds;
+  }
+}
