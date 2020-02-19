34030bbafe4e4221e674aca271cd07e4639c3d97
Sai Murthy <saimurthy66@gmail.com>
2020-02-18 19:26:41 -0800

    Fixes while testing

--- NRGRobot2020/src/main/java/frc/robot/subsystems/AcquirerPiston.java
+++ NRGRobot2020/src/main/java/frc/robot/subsystems/AcquirerPiston.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class AcquirerPiston extends SubsystemBase {
-    public DoubleSolenoid acquirerSolenoid = new DoubleSolenoid(0, 1);
+    public DoubleSolenoid acquirerSolenoid = new DoubleSolenoid(2, 3);
     public AcquirerPiston() {
     }
     
