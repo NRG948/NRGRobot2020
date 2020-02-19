915d9e8a345557457f01ba4951678d05e7c79a95
AdiBaddi <adhvitha.Sk85@gmail.com>
2020-02-18 20:34:00 -0800

    send hood encoder value to shuffleboard

--- NRGRobot2020/src/main/java/frc/robot/subsystems/Hood.java
+++ NRGRobot2020/src/main/java/frc/robot/subsystems/Hood.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AnalogEncoder;
+import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.TurretConstants;
@@ -11,19 +12,22 @@ import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.shuffleboard.SimpleWidget;
 
-
 public class Hood extends SubsystemBase {
+  
   private Victor hoodMotor = new Victor(TurretConstants.kHoodMotorPort);
-  private AnalogEncoder hoodEncoder;
+  private AnalogInput encoderInput = new AnalogInput(0);
+  private AnalogEncoder hoodEncoder = new AnalogEncoder(encoderInput);
   private double sentPower;
 
-  private SimpleWidget rawHoodOutputWidget;
+  private SimpleWidget rawHoodOutputWidget;  
+  private SimpleWidget distanceHoodOutputWidget; 
+
   /**
    * Creates a new Hood.
    */
   public Hood() {
-
   }
+
   //TODO: Add encoders for hard limit
   public void rawHood(double power){
     sentPower = power * 0.5;
@@ -32,6 +36,8 @@ public class Hood extends SubsystemBase {
   }
   @Override
   public void periodic() {
+    rawHoodOutputWidget.getEntry().setDouble(hoodEncoder.get());
+    distanceHoodOutputWidget.getEntry().setDouble(hoodEncoder.getDistance());
     // This method will be called once per scheduler run
   }
   
@@ -40,6 +46,7 @@ public class Hood extends SubsystemBase {
   
     ShuffleboardLayout hoodLayout = hoodTab.getLayout("Hood", BuiltInLayouts.kList).withPosition(0, 0).withSize(2, 4);
     rawHoodOutputWidget = hoodLayout.add("Raw Output", 0.0);
+    distanceHoodOutputWidget = hoodLayout.add("Distance", 0.0);
   }
   public void hoodEnd() {
     this.hoodMotor.set(0);
