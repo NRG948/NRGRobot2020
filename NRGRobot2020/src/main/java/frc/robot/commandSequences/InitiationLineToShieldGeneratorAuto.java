1bcf711703a3e124a0f00659e6148e514818670b
Naomi Bashkansky <nomichka@gmail.com>
2020-02-26 18:59:47 -0800

    Implement new acquirer sequence in InitiationLineToXXX

--- NRGRobot2020/src/main/java/frc/robot/commandSequences/InitiationLineToShieldGeneratorAuto.java
+++ NRGRobot2020/src/main/java/frc/robot/commandSequences/InitiationLineToShieldGeneratorAuto.java
@@ -7,8 +7,10 @@ import edu.wpi.first.wpilibj.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.AcquireNumberOfBalls;
+import frc.robot.commands.AutoFeeder;
 import frc.robot.commands.AutoTurnToHeading;
 import frc.robot.commands.FollowWaypoints;
+import frc.robot.commands.SetAcquirerState;
 import frc.robot.commands.SetStartPosition;
 import frc.robot.commands.ToggleAcquirerPiston;
 import frc.robot.commands.TurnTurretToAngle;
@@ -20,6 +22,7 @@ import frc.robot.subsystems.Feeder;
 import frc.robot.subsystems.LimelightVision;
 import frc.robot.subsystems.ShooterRPM;
 import frc.robot.subsystems.Turret;
+import frc.robot.subsystems.AcquirerPiston.State;
 
 /**
  * Autonomous command sequence moving the robot from the initiation line to
@@ -33,7 +36,7 @@ public class InitiationLineToShieldGeneratorAuto extends SequentialCommandGroup 
   ShooterRPM shooterRPM, Turret turret, LimelightVision limelightVision, AcquirerPiston acquirerPiston) {
     super(
       new SetStartPosition(drive, new Pose2d(3.3, -0.786, new Rotation2d(0))), 
-      new ToggleAcquirerPiston(acquirerPiston),
+      new SetAcquirerState(acquirerPiston, State.EXTEND),
       new FollowWaypoints(drive,
                           // Starting pose
                           new Pose2d( 3.3, -0.786, new Rotation2d(0)),
@@ -43,7 +46,9 @@ public class InitiationLineToShieldGeneratorAuto extends SequentialCommandGroup 
                           new Pose2d(6.107, -2.987, new Rotation2d(Math.toRadians(-120))),
                           // Drive forward
                           false)
-          .alongWith(new AcquireNumberOfBalls(acquirer, ballCounter).withRelativeCount(2).withTimeout(5)),
+          .alongWith(new AcquireNumberOfBalls(acquirer, ballCounter).withRelativeCount(2).withTimeout(5))
+          .alongWith(new AutoFeeder(ballCounter, feeder))
+          .andThen(new SetAcquirerState(acquirerPiston, State.RETRACT)),
         new AutoTurnToHeading(drive).toHeading(-75).withTolerance(2).withMaxPower(0.8)
           .alongWith(new TurnTurretToAngle(turret, 130)),
         new AutoShootSequence(4000, shooterRPM, turret, feeder, acquirer, ballCounter, limelightVision));
