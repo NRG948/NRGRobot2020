6a72a5617e91b84f9f23a123349ad016ef0009a7
Ed Reed <reed.ej@hotmail.com>
2020-03-01 16:16:34 -0800

    Remove unused constructor

--- NRGRobot2020/src/main/java/frc/robot/commandSequences/AutoShootSequence.java
+++ NRGRobot2020/src/main/java/frc/robot/commandSequences/AutoShootSequence.java
@@ -6,14 +6,8 @@ import frc.robot.commands.AutoFeedToShooter;
 import frc.robot.commands.AutoTurret;
 import frc.robot.commands.DisableShooterRPM;
 import frc.robot.commands.MaintainShooterRPM;
-import frc.robot.commands.WaitForBallReady;
 import frc.robot.commands.WaitForMinRPM;
-import frc.robot.subsystems.Acquirer;
-import frc.robot.subsystems.BallCounter;
-import frc.robot.subsystems.Feeder;
-import frc.robot.subsystems.LimelightVision;
-import frc.robot.subsystems.ShooterRPM;
-import frc.robot.subsystems.Turret;
+
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
@@ -22,35 +16,30 @@ public class AutoShootSequence extends SequentialCommandGroup {
   /**
    * Shoots up to 5 balls in an autonoumous mode.
    */
-  public AutoShootSequence(double rpm, ShooterRPM shooterRPM, Turret turret, Feeder feeder, Acquirer acquirer, BallCounter ballCounter) {
-
-    super( 
-      new MaintainShooterRPM(shooterRPM).atRpm(rpm).setAndExit()
-        .alongWith(new AutoTurret(turret)),
-      new WaitForMinRPM(rpm, shooterRPM),
-      // Release Ball
-      new AutoFeedToShooter(acquirer, feeder, ballCounter),
-      // Are you at target rpm?
-      new WaitForMinRPM(rpm, shooterRPM),
-      // Release Ball
-      new AutoFeedToShooter(acquirer, feeder, ballCounter),
-      // Are you at target rpm?
-      new WaitForMinRPM(rpm, shooterRPM),
-      // Release Ball
-      new AutoFeedToShooter(acquirer, feeder, ballCounter),
-      // Are you at target rpm?
-      new WaitForMinRPM(rpm, shooterRPM),
-      // Release Ball
-      new AutoFeedToShooter(acquirer, feeder, ballCounter),
-      // Are you at target rpm?
-      new WaitForMinRPM(rpm, shooterRPM),
-      // Release Ball
-      new AutoFeedToShooter(acquirer, feeder, ballCounter),
-      new DisableShooterRPM(shooterRPM)
-    );
-  }
-  
   public AutoShootSequence(double rpm, RobotSubsystems subsystems){
-    this(rpm, subsystems.shooterRPM, subsystems.turret, subsystems.feeder, subsystems.acquirer, subsystems.ballCounter);
+    super( 
+      new MaintainShooterRPM(subsystems.shooterRPM).atRpm(rpm).setAndExit()
+        .alongWith(new AutoTurret(subsystems.turret)),
+      new WaitForMinRPM(rpm, subsystems.shooterRPM),
+      // Release Ball
+      new AutoFeedToShooter(subsystems.acquirer, subsystems.feeder, subsystems.ballCounter),
+      // Are you at target rpm?
+      new WaitForMinRPM(rpm, subsystems.shooterRPM),
+      // Release Ball
+      new AutoFeedToShooter(subsystems.acquirer, subsystems.feeder, subsystems.ballCounter),
+      // Are you at target rpm?
+      new WaitForMinRPM(rpm, subsystems.shooterRPM),
+      // Release Ball
+      new AutoFeedToShooter(subsystems.acquirer, subsystems.feeder, subsystems.ballCounter),
+      // Are you at target rpm?
+      new WaitForMinRPM(rpm, subsystems.shooterRPM),
+      // Release Ball
+      new AutoFeedToShooter(subsystems.acquirer, subsystems.feeder, subsystems.ballCounter),
+      // Are you at target rpm?
+      new WaitForMinRPM(rpm, subsystems.shooterRPM),
+      // Release Ball
+      new AutoFeedToShooter(subsystems.acquirer, subsystems.feeder, subsystems.ballCounter),
+      new DisableShooterRPM(subsystems.shooterRPM)
+    );
   }
 }
