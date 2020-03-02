d1bbbc989f17b27497637d93023df620d87f7903
Paul Davis <pauldavis@google.com>
2020-03-01 16:27:06 -0800

    Add command logging

--- NRGRobot2020/src/main/java/frc/robot/commands/WaitForMinRPM.java
+++ NRGRobot2020/src/main/java/frc/robot/commands/WaitForMinRPM.java
@@ -1,29 +1,29 @@
-
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShooterRPM;
+import frc.robot.utilities.Logger;
 import frc.robot.utilities.NRGPreferences;
 
 public class WaitForMinRPM extends CommandBase {
 
-  ShooterRPM shooterRPM;
-  double rpm;
-  double rpmOffset;
-  /**
-   * Creates a new WaitForMinRPM.
-   */
+  private final ShooterRPM shooterRPM;
+
+  private double rpm;
+  private double rpmOffset;
+
+  /** Creates a new WaitForMinRPM command. */
   public WaitForMinRPM(double rpm, ShooterRPM shooterRPM) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(shooterRPM);
     this.shooterRPM = shooterRPM;
     this.rpm = rpm;
+    addRequirements(shooterRPM);
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     rpmOffset = NRGPreferences.WAIT_FOR_RPM_OFFSET.getValue();
+    Logger.commandInit(this, "offset:" + rpmOffset);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -31,14 +31,15 @@ public class WaitForMinRPM extends CommandBase {
   public void execute() {
   }
 
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-  }
-
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
     return shooterRPM.getActualRPM() >= rpm - rpmOffset;
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    Logger.commandEnd(this);
   }
 }
