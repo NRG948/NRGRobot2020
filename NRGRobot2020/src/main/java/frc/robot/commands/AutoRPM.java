ce429421d88601bd266290878ea812bb243eac8f
Paul Davis <pauldavis@google.com>
2020-03-07 13:18:28 -0800

    Don't set goal RPM when disabling AutoRPM; cleanups

--- NRGRobot2020/src/main/java/frc/robot/commands/AutoRPM.java
+++ NRGRobot2020/src/main/java/frc/robot/commands/AutoRPM.java
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ShooterRPM;
 import frc.robot.utilities.Logger;
 
+/**
+ * Automatically sets the Shooter wheel RPM based on the distance from the Limelight camera.
+ */
 public class AutoRPM extends CommandBase {
   private final ShooterRPM shooterRPM;
   private final boolean enable;
@@ -19,14 +22,17 @@ public class AutoRPM extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    shooterRPM.enableAutoRPM(enable);
-    shooterRPM.setGoalRPM(1000); // Get the shooter wheel started
     Logger.commandInit(this, "enable: " + enable);
+    shooterRPM.enableAutoRPM(enable);
+    if (enable) {
+      shooterRPM.setGoalRPM(2000); // Get the shooter wheel started
+    }
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    // ShooterRPM.periodic() does the work to maintain correct RPM speed.
   }
 
   // Called once the command ends or is interrupted.
