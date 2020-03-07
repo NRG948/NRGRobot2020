df4010d17ac1f7da866697a3b15d48d4d39c0a35
Paul Davis <pauldavis@google.com>
2020-03-07 14:20:16 -0800

    Update comments

--- NRGRobot2020/src/main/java/frc/robot/commands/AutoHood.java
+++ NRGRobot2020/src/main/java/frc/robot/commands/AutoHood.java
@@ -5,15 +5,15 @@ import frc.robot.subsystems.Hood;
 import frc.robot.utilities.Logger;
 
 /**
- * Automatically sets the Shooter Hood based on the distince from the Limelight
- * camera.
+ * Enables or disables automatic Shooter Hood positioning based on the distance
+ * from the target that is determined via the Limelight camera.
  */
 public class AutoHood extends CommandBase {
   private final Hood hood;
   private final boolean enable;
 
   /**
-   * Creates a new AutoHood.
+   * Creates a new AutoHood command.
    */
   public AutoHood(Hood hood, boolean enable) {
     this.hood = hood;
@@ -21,7 +21,6 @@ public class AutoHood extends CommandBase {
     addRequirements(hood);
   }
 
-  // Called when the command is initially scheduled.
   @Override
   public void initialize() {
     Logger.commandInit(this, "enable: " + enable);
@@ -32,7 +31,6 @@ public class AutoHood extends CommandBase {
     }
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
     // Hood.periodic() does the work to maintain correct Hood angle.
@@ -45,7 +43,7 @@ public class AutoHood extends CommandBase {
 
   }
 
-  // Returns true when the command should end.
+  // This command ends immediately because the real work is done in the Hood subsystem.
   @Override
   public boolean isFinished() {
     return true;
