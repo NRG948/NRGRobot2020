a6b87096a4c5738df3ec6a27d6bf4b6a694a2195
Paul Davis <pauldavis@google.com>
2020-03-08 11:41:20 -0700

    Log the rpm

--- NRGRobot2020/src/main/java/frc/robot/commands/SetApproximateShooterRPM.java
+++ NRGRobot2020/src/main/java/frc/robot/commands/SetApproximateShooterRPM.java
@@ -21,7 +21,7 @@ public class SetApproximateShooterRPM extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    Logger.commandInit(this);
+    Logger.commandInit(this, String.format("%.0f", rpm));
   }
 
   // Called every time the scheduler runs while the command is scheduled.
