2db830ae8a4cb0d395c6baa4eb79520817ecb1d5
Ed Reed <reed.ej@hotmail.com>
2020-03-08 11:30:58 -0700

    Implement alternate method of initialization path parameters to output to standard command logging.

--- NRGRobot2020/src/main/java/frc/robot/commandSequences/AutoDriveToLoadingStation.java
+++ NRGRobot2020/src/main/java/frc/robot/commandSequences/AutoDriveToLoadingStation.java
@@ -27,35 +27,44 @@ import frc.robot.vision.LoadingStationTarget;
 // information, see:
 // https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
 public class AutoDriveToLoadingStation extends SequentialCommandGroup {
+  private RaspberryPiVision raspberryPiVision;
+  private Drive drive;
+  private LoadingStationTarget target;
+  private double xOffset;
+  private double yOffset;
+  private Pose2d start;
+  private Pose2d end;
+  private Translation2d waypoint;
+
   /**
    * Creates a new AutoDriveToLoadingStation.
    */
-  public AutoDriveToLoadingStation(RaspberryPiVision raspberryPiVision, Drive drive, 
-      double xOffset, double yOffset) {
-    // Add your commands in the super() call, e.g.
-    // super(new FooCommand(), new BarCommand());
-    super(new SetRaspberryPiPipeline(raspberryPiVision, PipelineRunner.LOADING_STATION),
-        new WaitForNewVisionData(raspberryPiVision), 
-        new InstantCommand(() -> {
-          LoadingStationTarget target = raspberryPiVision.getLoadingTarget();
-          if (target != null) {
-            double heading = drive.getHeading();
-            Pose2d start = drive.getPose();
-            System.out.println("Start " + start);
-            Translation2d finalPoint = target.getFinalPoint(heading, xOffset, yOffset);
-            System.out.println("Final " + finalPoint);
-            Translation2d waypoint = target.getWaypoint(heading, xOffset, yOffset);
-            System.out.println("Waypoint " + waypoint);
-            Pose2d end = new Pose2d(start.getTranslation().plus(finalPoint), new Rotation2d());
-            new FollowWaypoints(drive, start, List.of(waypoint), end, false).schedule();
-            System.out.println("End " + end);
-          }
-        }));
+  public AutoDriveToLoadingStation(RaspberryPiVision raspberryPiVision, Drive drive, double xOffset, double yOffset) {
+    this.raspberryPiVision = raspberryPiVision;
+    this.drive = drive;
+    this.xOffset = xOffset;
+    this.yOffset = yOffset;
+
+    addCommands(new SetRaspberryPiPipeline(raspberryPiVision, PipelineRunner.LOADING_STATION),
+        new WaitForNewVisionData(raspberryPiVision).withTimeout(0.25), 
+        new InstantCommand(() -> scheduleFollowWaypoints()));
   }
 
   @Override
   public void initialize(){
-    Logger.commandInit(this);
+    LoadingStationTarget target = raspberryPiVision.getLoadingTarget();
+    if (target == null) {
+      Logger.commandInit(this, "NO TARGET");
+    } else {
+      double heading = drive.getHeading();
+      start = drive.getPose();
+      Translation2d finalPoint = target.getFinalPoint(heading, xOffset, yOffset);
+      waypoint = target.getWaypoint(heading, xOffset, yOffset);
+      end = new Pose2d(start.getTranslation().plus(finalPoint), new Rotation2d());
+    }
+
+    Logger.commandInit(this,
+      String.format("start: %s, waypoint: %s, end: %s ", start.toString(), waypoint.toString(), end.toString()));
     super.initialize();
   }
   
@@ -63,5 +72,11 @@ public class AutoDriveToLoadingStation extends SequentialCommandGroup {
   public void end(boolean interrupted){
     super.end(interrupted);
     Logger.commandEnd(this, interrupted);
+  }
+
+  private void scheduleFollowWaypoints() {
+    if (target != null) {
+      new FollowWaypoints(drive, start, List.of(waypoint), end, false).schedule();
+    }
   }
 }
